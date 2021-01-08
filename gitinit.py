2cbd905345b6067a673a605ed992a11f4c21416c
Jordan Raychev <jpraychev@gmail.com>
2021-01-08 17:15:27 +0200

    Changes

--- gitinit.py
+++ gitinit.py
@@ -1,12 +1,32 @@
+import subprocess
+
 # autocrlf: Input for linux, True for Windows/MacOS, False otherwise
 # editor: VSCode = code
+INFORMATION = {
+    'user.name' : [],
+    'user.email' : [],
+    'core.autocrlf' : [],
+    'core.editor' : [],
+}
 
-def gitinit():
-    print('A python module to initialize git for your current project')
-    username = input(f'Enter your name: ')
-    email = input(f'Enter your email address: ')
-    autocrlf = input(f'Enter your development OS (Linux/MacOS/Windows): ')
-	editor = input(f'Enter your development editor. See gitinit --help for supported editors: ')
-    
+def gitinit():   
+
+    print('Please enter your information below:')
+
+    INFORMATION['user.name'].append(input(f'\tUsername: '))
+
+    # email = input(f'\tEmail address: ')
+    # autocrlf = input(f'\tDevelopment OS (Linux/MacOS/Windows): ')
+    # editor = input(f'\tDevelopment editor: ')
+
+    # subprocess.run([
+    #     'git', 'config', '--global', 'user.name', f'{username}' 
+    # ])
+
 if __name__ == "__main__":
     gitinit()
+
+    # INFORMATION['username'].append('jraychev')
+    print(INFORMATION)
+
+    
