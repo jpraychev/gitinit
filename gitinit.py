a921b5ac3f46e3961c83b1724230d96514b51861
Jordan Raychev <jpraychev@gmail.com>
2021-01-11 10:02:18 +0200

    Base script

--- gitinit.py
+++ gitinit.py
@@ -1,32 +1,26 @@
 import subprocess
 
-# autocrlf: Input for linux, True for Windows/MacOS, False otherwise
+# autocrlf: input for linux, true for Windows/MacOS, false otherwise
 # editor: VSCode = code
 INFORMATION = {
-    'user.name' : [],
-    'user.email' : [],
-    'core.autocrlf' : [],
-    'core.editor' : [],
+    'Username' : ['user.name'],
+    'Email' : ['user.email'],
+    'Dev OS' : ['core.autocrlf'],
+    'Dev editor' : ['core.editor'],
 }
 
 def gitinit():   
 
     print('Please enter your information below:')
-
-    INFORMATION['user.name'].append(input(f'\tUsername: '))
-
-    # email = input(f'\tEmail address: ')
-    # autocrlf = input(f'\tDevelopment OS (Linux/MacOS/Windows): ')
-    # editor = input(f'\tDevelopment editor: ')
-
-    # subprocess.run([
-    #     'git', 'config', '--global', 'user.name', f'{username}' 
-    # ])
+    
+    for k in INFORMATION.keys():
+        INFORMATION[k].append(input(f'\t{k}: '))
+        subprocess.run([
+            'git', 'config', '--global', f'{INFORMATION[k][0]}', f'{INFORMATION[k][1]}' 
+        ])
 
 if __name__ == "__main__":
     gitinit()
 
-    # INFORMATION['username'].append('jraychev')
-    print(INFORMATION)
 
     
