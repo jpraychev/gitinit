845b25cd143221be74d16885fcb81fa69fbf643c
Jordan Raychev <jpraychev@gmail.com>
2021-01-11 16:39:40 +0200

    Basic checks for user provided OS

--- gitinit.py
+++ gitinit.py
@@ -2,7 +2,8 @@ import subprocess
 
 # autocrlf: input for linux, true for Windows/MacOS, false otherwise
 # editor: VSCode = code
-INFORMATION = {
+
+INFO = {
     'Username' : ['user.name'],
     'Email' : ['user.email'],
     'Dev OS' : ['core.autocrlf'],
@@ -11,13 +12,26 @@ INFORMATION = {
 
 def gitinit():   
 
-    print('Please enter your information below:')
+    print('Please enter your INFO below:')
     
-    for k in INFORMATION.keys():
-        INFORMATION[k].append(input(f'\t{k}: '))
+    for k in INFO.keys():
+        INFO[k].append(input(f'\t{k}: '))
+
+        # Basic checks for user provided operating system
+        
+        if k == 'Dev OS':
+            if INFO[k][1].lower() == 'linux':
+                INFO[k][1] = 'input'
+            elif INFO[k][1].lower() == 'windows':
+                INFO[k][1] = 'true'
+            else:
+                INFO[k][1] = 'false'
+
         subprocess.run([
-            'git', 'config', '--global', f'{INFORMATION[k][0]}', f'{INFORMATION[k][1]}' 
+            'git', 'config', '--global', f'{INFO[k][0]}', f'{INFO[k][1]}' 
         ])
+
+        
 
 if __name__ == "__main__":
     gitinit()
